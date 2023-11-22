5c75b4b243ea50c535f788b2572a18b1ef53461d
TomNelsonTembo <neltembo2002@gmail.com>
2023-11-22 10:18:59 +0100

    Made Update to the Graphics

--- test.py
+++ test.py
@@ -7,17 +7,26 @@ screen = pygame.display.set_mode((800, 400))
 open = True
 pygame.display.set_caption('Runner 2D')
 clock = pygame.time.Clock()
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load ('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load ('graphics/ground.png').convert()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 text_surface = test_font.render('Runner 2D', False, 'Black')
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
-snail_x_pos = 600
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom = (900, 300))
 
 
-player_surface = pygame.image.load('graphics/Player/player_walk_1.png')
-player_x_pos = 50
+
+player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (50, 300))
+
+
+
+
+
+
+
+
 
 while open:
     for event in pygame.event.get():
@@ -25,16 +34,16 @@ while open:
             pygame.quit()
             exit()
     
-    snail_x_pos -= 3
-    if snail_x_pos <= -100:
-        snail_x_pos = 800
+    snail_rect.left -= 3
+    if snail_rect.right <= -100:
+        snail_rect.right = 900
         
-    
+    print(player_rect.left)
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
-    screen.blit(snail_surface, (snail_x_pos, 263))
-    screen.blit(player_surface, (player_x_pos, 217))
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surface, player_rect)
     pygame.display.update()
     clock.tick(60)
 
